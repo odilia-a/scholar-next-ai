ab2163cb412dd67d5b5b448e63edb254c758b7bd
odilia-a <osiosesharon@gmail.com>
2026-05-22 17:23:03 +0100

    debug render port issue

--- src/middleware/sanitize.middleware.js
+++ src/middleware/sanitize.middleware.js
@@ -1,8 +1,16 @@
-const mongoSanitize = require("express-mongo-sanitize");
+const expressMongoSanitize = require("express-mongo-sanitize");
 const xss = require("xss-clean");
 
 const sanitize = (app) => {
-  app.use(mongoSanitize()); // prevents NoSQL injection
+  app.use((req, res, next) => {
+    ["body", "params", "headers", "query"].forEach((key) => {
+      if (req[key] && typeof expressMongoSanitize.sanitize === "function") {
+        expressMongoSanitize.sanitize(req[key]);
+      }
+    });
+    next();
+  });
+
   app.use(xss()); // prevents script injection
 };
 
